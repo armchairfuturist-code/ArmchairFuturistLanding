2c6a4272b5a767028450442260f993b5d914b202
Alex Myers <armchairfuturist@gmail.com>
2025-05-27 08:35:02 +0000

    ok - please add this testimonial, I do not see it yet
    Also - I am not Seeing Evan's testimonial when the scrolling finishes, ensure all testimonials have enough time to be seen!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,9 +99,10 @@ export default {
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
-        marquee: 'marquee 20s linear infinite',
+        marquee: 'marquee 50s linear infinite',
   		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
+
