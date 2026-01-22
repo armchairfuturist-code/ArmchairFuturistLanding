4d3721bec079e292969fa801e89bd62b535abcf2
Armchair Futurist <armchairfuturist@google.com>
2026-01-22 11:29:57 +0000

    test

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAnalytics, Analytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBug7-9Btt0pQZ7XsJGLe5w3W4jrrQRYuc",
-    authDomain: "armchair-futurist.firebaseapp.com",
-    projectId: "armchair-futurist",
-    storageBucket: "armchair-futurist.firebasestorage.app",
-    messagingSenderId: "1083895638095",
-    appId: "1:1083895638095:web:e39db31bff1d819c5d623a",
-    measurementId: "G-18FCVPH408"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
